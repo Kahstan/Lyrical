347ceb7afde9fe3905ef8ccfa2693ea2ce0014c1
kahstan <kahsheng@gmail.com>
2022-06-26 13:05:56 +0800

    Changing some CSS components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from "./components/Form";
 function App() {
   return (
     <div>
-      <h1>Search Lyrics</h1>
+      <h1 className="text-5xl font-medium py-2">Search Lyrics</h1>
       <Form />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -33,27 +33,41 @@ const Form = () => {
     fetchPost(apiUrl + artist + "/" + song);
   };
 
+  const artistSong = artist + " - " + song;
+
   return (
-    <>
+    <div>
       <form>
-        <input
-          type="text"
-          placeholder="Artist name"
-          onChange={handleArtistChange}
-        />
+        <span>
+          <input
+            type="text"
+            className="mt-1 block w-50 px-3 mx-1 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 capitalize
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500 invalid:text-pink-60 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 placeholder:italic"
+            placeholder="Artist"
+            onChange={handleArtistChange}
+          />
+        </span>
+        <span>
+          <input
+            type="text"
+            className="mt-1 block w-50 px-3 py-2 mx-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 capitalize
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 placeholder:italic
+        "
+            placeholder="Song name"
+            onChange={handleSongChange}
+          />
+        </span>
 
-        <input
-          type="text"
-          placeholder="Song name"
-          onChange={handleSongChange}
-        />
-
-        <button type="submit" onClick={handleSubmit}>
+        <button
+          className="py-2 px-3 w-50 mx-1 block bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          onClick={handleSubmit}
+        >
           Search
         </button>
       </form>
-      <p>{lyrics}</p>
-    </>
+      <h2 className="capitalize mt-2 text-3xl">{artistSong}</h2>
+      <p className="ml-24 mt-10 text-2xl max-w-xl max-h-m">{lyrics}</p>
+    </div>
   );
 };
 
