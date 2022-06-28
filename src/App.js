045725537d53d3c0d173b9f51709dce5f650094a
kahstan <kahsheng@gmail.com>
2022-06-28 14:14:58 +0800

    Made one seach bar for both elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Lyrics from "./components/Lyrics";
 import Youtube from "./components/Youtube";
 
 const apiUrl = "https://api.lyrics.ovh/v1/";
+const APIKEY = "AIzaSyDkfCnshU7ku2AMAvsLa7U4SfED2ZO84ws";
+const result = 5;
 
 function App() {
   const [artist, setArtist] = useState("");
   const [song, setSong] = useState("");
   const [lyrics, setLyrics] = useState("");
   const [error, setError] = useState(null);
+  //// Youtube////
+  const [search, setSearch] = useState([]);
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [firstLoad, setFirstLoad] = useState(true);
+  /////// youtube final api ////////
+  const finalUrl = `https://youtube.googleapis.com/youtube/v3/search?q=${input}&key=${APIKEY}&maxResults=${result}`;
 
   const fetchLyrics = async (url) => {
     setError(null);
@@ -25,16 +34,71 @@ function App() {
   };
 
   const handleArtistChange = (event) => {
+    event.preventDefault();
     setArtist(event.target.value);
   };
   const handleSongChange = (event) => {
+    event.preventDefault();
     setSong(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetchLyrics(apiUrl + artist + "/" + song);
+    setInput(artist + "%20" + song);
   };
+
+  const fetchSearch = async (url) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const res = await fetch(url);
+
+      if (res.status !== 200) {
+        throw new Error("something is wrong");
+      }
+
+      const data = await res.json();
+      const mapData = data.items.map(
+        (obj) => "https://www.youtube.com/embed/" + obj.id.videoId
+      );
+
+      const finalMapData = mapData.map((link, i) => {
+        return (
+          <div className="youtube" key={i}>
+            <iframe
+              className="ml-10 relative"
+              width="560"
+              height="315"
+              key={i}
+              src={link}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            ></iframe>
+            <br />
+          </div>
+        );
+      });
+
+      setSearch(finalMapData);
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    if (!firstLoad) {
+      fetchLyrics(apiUrl + artist + "/" + song);
+      fetchSearch(finalUrl);
+    } else {
+      setFirstLoad(false);
+    }
+  }, [input]);
 
   return (
     <div>
@@ -48,7 +112,11 @@ function App() {
       />
       <div className="grid grid-cols-6 gap-4 border- 2">
         <div className=" block mt-4 mb-10">
-          <Youtube />
+          <Youtube
+            handleInputChange={handleInputChange}
+            handleYoutubeSubmit={handleSubmit}
+          />
+          <div>{search}</div>
         </div>
         {/* this is scary */}
         <p
--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,81 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
-const APIKEY = "AIzaSyBYqcs4yJLvZWb6ZJK5iEDi_KZn1Hog_Kw";
-const result = 5;
-
-const Youtube = () => {
-  const [search, setSearch] = useState([]);
-  const [input, setInput] = useState("");
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const finalUrl = `https://youtube.googleapis.com/youtube/v3/search?q=${input}&key=${APIKEY}&maxResults=${result}`;
-
-  const fetchSearch = async (url) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const res = await fetch(url);
-
-      if (res.status !== 200) {
-        throw new Error("something is wrong");
-      }
-
-      const data = await res.json();
-      console.log(data.items[0].id.videoId);
-      const mapData = data.items.map(
-        (obj) => "https://www.youtube.com/embed/" + obj.id.videoId
-      );
-
-      const finalMapData = mapData.map((link, i) => {
-        return (
-          <div className="youtube" key={i}>
-            <iframe
-              className="ml-10 relative"
-              width="560"
-              height="315"
-              key={i}
-              src={link}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-            <br />
-          </div>
-        );
-      });
-
-      console.log(finalMapData);
-      setSearch(finalMapData);
-    } catch (err) {
-      setError(err.message);
-    }
-    setIsLoading(false);
-  };
-
-  const handleInputChange = (event) => {
-    setInput(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetchSearch(finalUrl);
-  };
+const Youtube = (props) => {
   return (
     <div>
       <form className="ml-40 mb-10">
         <input
           placeholder="Youtube Search"
-          onChange={handleInputChange}
+          onChange={props.handleInputChange}
         ></input>
         <button
           className="py-2 px-3 w-50 mx-1 block bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          onClick={handleSubmit}
+          onClick={props.handleSubmit}
         >
           Search
         </button>
       </form>
-      <div>{search}</div>
       {/* {search.mapData.map((link, i) => {
         const frame = (
           <div className="youtube">
